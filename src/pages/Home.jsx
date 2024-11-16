430f7b8ad976f8ad1ee6901558d21a9be13b217e
Harshit Vishwakarma <harshitvishwakarma007@gmail.com>
2024-11-16 22:35:55 +0530

    new changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import sideimage from '../assets/sideimage1.jpg'
 
 
 const HomePage = () => {
@@ -10,13 +11,13 @@ const HomePage = () => {
         <div className="absolute inset-0 bg-black bg-opacity-20 h-full"></div>
 
         <div className='w-full z-50'>
-          <div>
+          <div className='text-slate-300'>
             <h1 className='font-extrabold text-center text-[3rem]'>MSB DECORATIVES AND FURNITURE</h1>
             <h2 className='font-bold text-center'>To make life luxury for you.</h2>
           </div>
           <div className='flex items-center justify-center relative top-[35%]'>
-            <button className='text-3xl bg-gray-800 p-2 rounded-md hover:bg-gray-950 hover:text-2xl ease-in transition-all'>
-              Click me!
+            <button className='text-3xl bg-gray-800 p-2 text-white rounded-md hover:bg-gray-950 hover:text-2xl ease-in transition-all'>
+              Book a call!
             </button>
           </div>
         </div>
@@ -24,6 +25,16 @@ const HomePage = () => {
 
       <section className=' bg-gray-800 h-[500px]'>
         <h1 className='text-center text-[2rem] font-bold text-slate-50'>Deal with us! Make your business Luxury.</h1>
+        <div className='flex justify-between mt-4'>
+          <div className='flex-1 p-4'>
+            <p>
+              MSB Decoratives and Furniture is Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sit sapiente dolorum temporibus aliquid labore, architecto velit cum ut vero blanditiis eaque iusto et aliquam id consectetur numquam! Eos nesciunt error iste deleniti eaque magni nam excepturi autem harum, sit illo aliquam consequuntur veniam asperiores quia voluptate ab repellat assumenda reiciendis adipisci ratione nisi sequi veritatis culpa? Atque autem nihil voluptate accusantium tenetur architecto provident, unde enim, recusandae velit veritatis.
+            </p>
+          </div>
+          <div className='flex-1 p-4'>
+            <img src={sideimage} alt="" />
+          </div>
+        </div>
       </section>
     </>
   );
